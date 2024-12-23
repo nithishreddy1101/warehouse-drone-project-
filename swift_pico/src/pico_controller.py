7ccd8333999b8244c7b0fa4c6a3920683863bc21
Nithish Reddy <127601258+nithishreddy1101@users.noreply.github.com>
2024-12-23 20:06:20 +0530

    Update pico_controller.py

--- swift_pico/src/pico_controller.py
+++ swift_pico/src/pico_controller.py
@@ -22,7 +22,6 @@ from pid_msg.msg import PIDTune, PIDError
 import rclpy
 from rclpy.node import Node
 
-
 class Swift_Pico(Node):
 	def __init__(self):
 		super().__init__('pico_controller')  # initializing ros node with name pico_controller
@@ -32,8 +31,7 @@ class Swift_Pico(Node):
 		self.drone_position = [0.0, 0.0, 0.0]
 
 		# [x_setpoint, y_setpoint, z_setpoint]
-		self.setpoint = [2, 2, 20]  # whycon marker at the position of the dummy given in the scene. Make the whycon marker associated with position_to_hold dummy renderable and make changes accordingly
-
+		self.setpoint = [2.0, 2.0, 17.5]  # whycon marker at the position of the dummy given in the scene. Make the whycon marker associated with position_to_hold dummy renderable and make changes accordingly
 		# Declaring a cmd of message type swift_msgs and initializing values
 		self.cmd = SwiftMsgs()
 		self.cmd.rc_roll = 1500
@@ -48,11 +46,19 @@ class Swift_Pico(Node):
 		self.Ki = [0, 0, 0]
 		self.Kd = [0, 0, 0]
 
+		self.Kp = [1000*0.02, 1000*0.02, 901*0.02]
+		self.Ki = [0.0001*3, 0.0001*3, 0.00]
+		self.Kd = [1280*0.6, 1280*0.6, 1262*0.6]
+
+
 		#-----------------------Add other required variables for pid here ----------------------------------------------
-
-
-
-
+		self.max_values = [2000,2000,2000]
+		self.min_values = [1000,1000,1000]
+		self.previous_error=[0.0,0.0,0.0]
+		self.pid_error=PIDError()
+		self.iterm=[0.0,0.0,0.0]
+		self.error=[0.0,0.0,0.0]
+		self.diff =[0.0,0.0,0.0]
 
 
 
@@ -65,7 +71,7 @@ class Swift_Pico(Node):
 
 		# # This is the sample time in which you need to run pid. Choose any time which you seem fit.
 	
-		self.sample_time = 0.060  # in seconds
+		self.sample_time = 0.060 # in seconds
 
 		# Publishing /drone_command, /pid_error
 		self.command_pub = self.create_publisher(SwiftMsgs, '/drone_command', 10)
@@ -79,10 +85,11 @@ class Swift_Pico(Node):
 		self.create_subscription(PIDTune, "/throttle_pid", self.altitude_set_pid, 1)
 
 		#------------------------Add other ROS Subscribers here-----------------------------------------------------
-	
+		self.create_subscription(PIDTune, "/pitch_pid", self.pitch_set_pid, 1)
+		self.create_subscription(PIDTune, "/roll_pid", self.roll_set_pid, 1)
 
 		self.arm()  # ARMING THE DRONE
-
+		self.create_timer(self.sample_time,self.pid)
 		# Creating a timer to run the pid function periodically, refer ROS 2 tutorials on how to create a publisher subscriber(Python)
 
 
@@ -105,64 +112,77 @@ class Swift_Pico(Node):
 		self.command_pub.publish(self.cmd)  # Publishing /drone_command
 
 
-	# Whycon callback function
-	# The function gets executed each time when /whycon node publishes /whycon/poses 
 	def whycon_callback(self, msg):
 		self.drone_position[0] = msg.poses[0].position.x 
-		#--------------------Set the remaining co-ordinates of the drone from msg----------------------------------------------
-
-
-
-	
-		#---------------------------------------------------------------------------------------------------------------
+		self.drone_position[1] = msg.poses[0].position.y
+		self.drone_position[2] = msg.poses[0].position.z
 
 
 	# Callback function for /throttle_pid
 	# This function gets executed each time when /drone_pid_tuner publishes /throttle_pid
 	def altitude_set_pid(self, alt):
-		self.Kp[2] = alt.kp * 0.03  # This is just for an example. You can change the ratio/fraction value accordingly
-		self.Ki[2] = alt.ki * 0.008
+		self.Kp[2] = alt.kp * 0.02 
+		self.Ki[2] = alt.ki * 0.0001
 		self.Kd[2] = alt.kd * 0.6
+	
+	def pitch_set_pid(self,alt):
+		self.Kp[1] = alt.kp * 0.02
+		self.Ki[1] = alt.ki * 0.0001
+		self.Kd[1] = alt.kd * 0.6
 
-	#----------------------------Define callback function like altitide_set_pid to tune pitch, roll--------------
-
-	#----------------------------------------------------------------------------------------------------------------------
-
+	def roll_set_pid(self,alt):
+		self.Kp[0] = alt.kp * 0.02
+		self.Ki[0] = alt.ki * 0.0001
+		self.Kd[0] = alt.kd * 0.6
 
 	def pid(self):
-	#-----------------------------Write the PID algorithm here--------------------------------------------------------------
 
-	# Steps:
-	# 	1. Compute error in each axis. eg: error[0] = self.drone_position[0] - self.setpoint[0] ,where error[0] corresponds to error in x...
-	#	2. Compute the error (for proportional), change in error (for derivative) and sum of errors (for integral) in each axis. Refer "Understanding PID.pdf" to understand PID equation.
-	#	3. Calculate the pid output required for each axis. For eg: calcuate self.out_roll, self.out_pitch, etc.
-	#	4. Reduce or add this computed output value on the avg value ie 1500. For eg: self.cmd.rcRoll = 1500 + self.out_roll. LOOK OUT FOR SIGN (+ or -). EXPERIMENT AND FIND THE CORRECT SIGN
-	#	5. Don't run the pid continously. Run the pid only at the a sample time. self.sampletime defined above is for this purpose. THIS IS VERY IMPORTANT.
-	#	6. Limit the output value and the final command value between the maximum(2000) and minimum(1000)range before publishing. For eg : if self.cmd.rcPitch > self.max_values[1]:
-	#																														self.cmd.rcPitch = self.max_values[1]
-	#	7. Update previous errors.eg: self.prev_error[1] = error[1] where index 1 corresponds to that of pitch (eg)
-	#	8. Add error_sum
+		self.error[0] = self.drone_position[0]-self.setpoint[0]
+		self.error[1] = self.drone_position[1]-self.setpoint[1]
+		self.error[2] = self.drone_position[2]-self.setpoint[2]
+		self.iterm[0]=self.iterm[0]+self.error[0]
+		self.iterm[1]=self.iterm[1]+self.error[1]
+		self.iterm[2]=self.iterm[2]+self.error[2]
+		self.diff[0]=self.error[0]-self.previous_error[0]
+		self.diff[1]=self.error[1]-self.previous_error[1]
+		self.diff[2]=self.error[2]-self.previous_error[2]
 
+		self.get_logger().info(f" drone_pos ::{self.drone_position}")
+	
+		self.out_roll     = int(self.Kp[0]*self.error[0]+self.Kd[0]*self.diff[0]+self.Ki[0]*self.iterm[0])
+		self.out_pitch    = int(self.Kp[1]*self.error[1]+self.Kd[1]*self.diff[1]+self.Ki[1]*self.iterm[1])
+		self.out_throttle = int(self.Kp[2]*self.error[2]+self.Kd[2]*self.diff[2]+self.Ki[2]*self.iterm[2])
 
+		# self.get_logger().info(f" iterm ::{self.iterm[2]} i_error {self.Ki[2]*self.iterm[2]}")
 
+		self.cmd.rc_throttle = 1500+self.out_throttle
+		self.cmd.rc_roll = 1500-self.out_roll
+		self.cmd.rc_pitch = 1500+self.out_pitch
 
+		if self.cmd.rc_roll > self.max_values[0]:
+			self.cmd.rc_roll = self.max_values[0]
+		if self.cmd.rc_pitch > self.max_values[1]:
+			self.cmd.rc_pitch = self.max_values[1]
+		if self.cmd.rc_throttle > self.max_values[2]:
+			self.cmd.rc_throttle = self.max_values[2]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-	#------------------------------------------------------------------------------------------------------------------------
+		if self.cmd.rc_roll <self.min_values[0]:
+			self.cmd.rc_roll = self.min_values[0]
+		if self.cmd.rc_pitch < self.min_values[1]:
+			self.cmd.rc_pitch = self.min_values[1]
+		if self.cmd.rc_throttle < self.min_values[2]:
+			self.cmd.rc_throttle = self.min_values[2]
+		
+		self.previous_error[0]=self.error[0]
+		self.previous_error[1]=self.error[1]
+		self.previous_error[2]=self.error[2]
+		
 		self.command_pub.publish(self.cmd)
-		# calculate throttle error, pitch error and roll error, then publish it accordingly
+
+		self.pid_error.roll_error=self.error[0]
+		self.pid_error.pitch_error=self.error[1]
+		self.pid_error.throttle_error= self.error[2]
+
 		self.pid_error_pub.publish(self.pid_error)
 
 
